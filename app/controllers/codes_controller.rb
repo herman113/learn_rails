0eac5fc9ba5f3c901cd9a3760b347f04b2018ff8
Herman <hermansoto@me.com>
2016-08-02 18:58:20 +0000

    add views and codes controller

---
+++ app/controllers/codes_controller.rb
@@ -0,0 +1,34 @@
+class CodesController < ApplicationController
+  
+  def home
+  end
+  def bootstrap
+  end
+
+  def controller
+  end
+
+  def forms
+  end
+
+  def generators
+  end
+
+  def git
+  end
+
+  def github
+  end
+
+  def heroku
+  end
+
+  def console
+  end
+
+  def routes
+  end
+
+  def views
+  end
+end
